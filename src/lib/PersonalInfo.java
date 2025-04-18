dc17ead081d818f3b107dd7dc6d70dce2bfff95d
Rosyidmw <rosyidmktwbw@gmail.com>
2025-04-18 16:33:53 +0700

    Refactor Data Clump di Employee: Memindahkan field informasi pribadi ke kelas PersonalInfo
    Refaktor ini dilakukan untuk mengatasi code smell berupa data clump pada kelas Employee, di mana beberapa field seperti firstName, lastName, idNumber, dan address sering digunakan bersama-sama dan mewakili informasi pribadi karyawan.
    
    Untuk meningkatkan keterbacaan dan pemeliharaan kode, field-field tersebut dipindahkan ke dalam kelas baru bernama PersonalInfo. Kelas Employee kini menyimpan satu instance PersonalInfo, menggantikan field-field tersebut.
    
    Dengan refaktor ini:
    
    - Kode menjadi lebih modular dan terstruktur.
    - Pemisahan tanggung jawab antara data pribadi dan data pekerjaan lebih jelas.
    - Potensi duplikasi kode dapat diminimalisasi jika informasi pribadi digunakan di tempat lain.

---
+++ src/lib/PersonalInfo.java
@@ -0,0 +1,15 @@
+package lib;
+
+public class PersonalInfo {
+    public String firstName;
+    public String lastName;
+    public String idNumber;
+    public String address;
+
+    public PersonalInfo(String firstName, String lastName, String idNumber, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = idNumber;
+        this.address = address;
+    }
+}
